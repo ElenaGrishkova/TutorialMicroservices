730de73574ce63e6612cfe3bc7298bf5767d3abb
LeGo <lmetis@rambler.ru>
2021-05-31 17:03:45 +0300

    Задание модуля 1. Создать простейший микросервис используя Spring Boot c REST API GET /greetings по иструкции https://spring.io/guides/gs/rest-service/)

---
+++ src/main/java/ru/fiasoft/tutorial/microservice/module1/Module1SpringBootApplication.java
@@ -0,0 +1,13 @@
+package ru.fiasoft.tutorial.microservice.module1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module1SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Module1SpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/fiasoft/tutorial/microservice/module1/Module1SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fiasoft.tutorial.microservice.module1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module1SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
